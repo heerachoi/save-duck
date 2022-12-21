3d0abbf1882b3ef1bc6f9d4c507ab6dfc8bcce7d
Minsung Kim <kim_minsung@@outlook.com>
2022-12-21 14:36:49 +0900

    ShopGuide Header 수정 후 테스트

--- src/pages/ShopGuide.jsx
+++ src/pages/ShopGuide.jsx
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+
 const ShopGuide = () => {
-  return <div>ShopGuide</div>;
+  return (
+    <StShopGuideContainer>
+      <StShopGuideHeader>
+        <StShopGuideHeaderLeft>
+          <h1> Shopping Guide</h1>
+        </StShopGuideHeaderLeft>
+      </StShopGuideHeader>
+    </StShopGuideContainer>
+
+
+  )
 };
+
+const StShopGuideContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StShopGuideHeader = styled.div`
+  color: black;
+  font-size: 1rem;
+  `;
+
+const StShopGuideHeaderLeft = styled.div`
+  `;
 
 export default ShopGuide;
