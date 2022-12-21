eed037ebf2d52a368207825504e8e05840603507
u_ju <89282724+Sangwoo950@users.noreply.github.com>
2022-12-21 22:37:51 +0900

    CSS 초기화 전 커밋

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div className="login__Contaier">
+        <h2>Login</h2>
+        이메일 주소
+        <input />
+        비밀번호
+        <input />
+        <button>로그인</button>
+        <button>회원가입</button>
+        <button>구글로 로그인</button>
+        <button>Github로 로그인</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
