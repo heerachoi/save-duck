f7f227860199b47e49b6c1a9bd7643e88270aab8
Hoesu Ji <newasborn@naver.com>
2022-12-22 13:50:20 +0900

    [Feat] 게시글 영역 생성

--- src/pages/ShopGuideDetails.jsx
+++ src/pages/ShopGuideDetails.jsx
@@ -1,5 +1,36 @@
+import styled from 'styled-components';
+
 const ShopGuideDetails = () => {
-  return <div>ShopGuideDetails</div>;
+  return (
+    <StShopDetailsContainer>
+      <StShopDetailsArticle>
+        <h3>게시글의 제목이 들어갑니다.</h3>
+        {/* <DetailsImageContainer> */}
+        <DetailsImage
+          className='detailsMainImage'
+          src='images/shop_guide_detail_image.jpg'
+          alt='떡볶이 사진'
+        />
+        {/* </DetailsImageContainer> */}
+        <div>게시글의 내용이 들어갑니다.</div>
+      </StShopDetailsArticle>
+    </StShopDetailsContainer>
+  );
 };
 
 export default ShopGuideDetails;
+
+const StShopDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StShopDetailsArticle = styled.div`
+  text-align: center;
+`;
+
+const DetailsImage = styled.img`
+  width: 500px;
+`;
