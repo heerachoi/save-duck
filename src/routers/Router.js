10011842fac01354f71d30314bc0efe240c6340d
u_ju <89282724+Sangwoo950@users.noreply.github.com>
2022-12-23 09:34:49 +0900

    [feat] Login page

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -4,19 +4,19 @@ import Navbar from '../Component/navBar/NavBar.jsx';
 import Home from '../pages/Home.jsx';
 import ShopGuide from '../pages/ShopGuide.jsx';
 import ShopGuideDetails from '../pages/ShopGuideDetails.jsx';
-import Login from '../pages/Login.jsx';
-import SignUp from '../pages/SignUp.jsx';
+import SignIn from "../pages/SignIn.jsx";
+import SignUp from "../pages/SignUp.jsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/shopguide' element={<ShopGuide />} />
-        <Route path='/shopguidedetails' element={<ShopGuideDetails />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/shopguide" element={<ShopGuide />} />
+        <Route path="/shopguidedetails" element={<ShopGuideDetails />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
