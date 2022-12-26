8c2577a1a7eac8c9ca245349fe7e22a12edaa7a1
heerachoi <heera7722@gmail.com>
2022-12-26 22:24:20 +0900

    [feat] 달력클릭시 아이템 목록 렌더링, 아이템 추가/삭제

--- src/Component/shoppingList/ShoppingList.jsx
+++ src/Component/shoppingList/ShoppingList.jsx
@@ -1,7 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { db } from '../../firebase.js';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, addDoc, getDocs, orderBy, query, where } from 'firebase/firestore';
+
+import { v4 as uuidv4 } from 'uuid';
 import { faPencil, faX, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { addList, loadList } from '../../redux/modules/shoppingListActions.js';
+import ShoppingItem from './ShoppingItem.jsx';
 
 import {
   ShoppingListContainer,
@@ -10,74 +16,133 @@ import {
   ShoppingListTitle,
   UncheckedList,
   ListItem,
-  CheckBox,
-  ItemName,
-  ItemPrice,
   CheckedList,
-  PencilIcon,
   XIcon,
   CheckIcon,
-  ItemPriceContainer,
+  ItemInput,
+  ItemPriceInput,
+  ItemPriceInputContainer,
   CheckListTotalContainer,
   CheckListTotalText,
   CheckListTotal,
   OverallTotalContainer,
   OverallTotalTotalText,
   OverallTotal,
-  ShowItem,
-  UnshowItem,
   ScrollBox,
+  ItemPriceContainerForm,
 } from './ShoppingList.js';
 
 const ShoppingList = ({ year, month, date }) => {
   const currentYear = year;
   const currentMonth = month + 1;
   const currentDate = date;
-
-  // const [item, setItem] = useState([]);
-  const [itemList, setItemList] = useState(['당근']);
-
+  // 현재 날자 - dbCollection
   const dateToString = '' + currentYear + currentMonth + currentDate;
-  const dateCollectionRef = collection(db, dateToString);
+  // console.log('dateToString');
+  // console.log(dateToString);
 
-  const syncShoppingItemListStateWithFirestore = () => {
-    getDocs(collection(db, dateToString)).then((querySnapshop) => {
+  // item
+  const [item, setItem] = useState('');
+  const [cost, setCost] = useState('');
+  const [itemList, setItemList] = useState([]);
+  const [checkedItemList, setCheckedItemList] = useState([]);
+  const [check, setCheck] = useState(false);
+
+  const dispatch = useDispatch();
+  // useEffect(() => {}, [itemList, checkedItemList]); // 댓글 등록 버튼 - 클릭시 댓글 리스트에 작성한 댓글 추가
+  const itemSubmitHandler = (event) => {
+    event.preventDefault();
+    const newItem = {
+      id: uuidv4(),
+      item,
+      date,
+      price: 0,
+      isChecked: false,
+    };
+    dispatch(addList(newItem));
+    setItem('');
+    setCost('');
+  };
+
+  // 아이템 작성 인풋창 내용 입력 시 state 업데이트
+  const itemChangeHandler = (event) => {
+    setItem(event.target.value);
+  };
+
+  // 아이템 작성 인풋창 내용 입력 시 state 업데이트
+  const priceChangeHandler = (event) => {
+    setCost(event.target.value);
+  };
+
+  const shoppingListUnchecked = () => {
+    const q = query(collection(db, dateToString), where('isChecked', '==', false));
+
+    getDocs(q).then((querySnapshop) => {
       const firestoreShoppingItemList = [];
       querySnapshop.forEach((doc) => {
         firestoreShoppingItemList.push({
+          id: doc.id,
           date: doc.data().date,
-          item: doc.data().item,
+          name: doc.data().name,
           isChecked: doc.data().isChecked,
           price: doc.data().price,
         });
       });
+      // console.log('firestoreShoppingItemList');
+      // console.log(firestoreShoppingItemList);
       setItemList(firestoreShoppingItemList);
     });
   };
 
+  const shoppingListChecked = () => {
+    const q = query(collection(db, dateToString), where('isChecked', '==', true));
+
+    getDocs(q).then((querySnapshop) => {
+      const firestoreShoppingItemListChecked = [];
+      querySnapshop.forEach((doc) => {
+        firestoreShoppingItemListChecked.push({
+          id: doc.id,
+          date: doc.data().date,
+          name: doc.data().name,
+          isChecked: doc.data().isChecked,
+          price: doc.data().price,
+        });
+      });
+      // console.log('firestoreShoppingItemListChecked');
+      // console.log(firestoreShoppingItemListChecked);
+      setCheckedItemList(firestoreShoppingItemListChecked);
+    });
+  };
+
   useEffect(() => {
-    syncShoppingItemListStateWithFirestore();
-  }, []);
+    shoppingListUnchecked();
+    shoppingListChecked();
+  }, [dateToString]);
 
   const addItem = async (newShoppingItem) => {
     const docRef = await addDoc(collection(db, dateToString), {
       date: dateToString,
-      item: '고기',
+      name: item,
       isChecked: false,
-      price: 3000,
+      price: cost,
+      modify: false,
     });
     setItemList([
       ...itemList,
       {
         date: dateToString,
-        item: '고기',
+        name: item,
         isChecked: false,
-        price: 3000,
+        price: cost,
+        modify: false,
       },
     ]);
-    // console.log('docRef');
-    // console.log(docRef);
   };
+
+  // 체크 되지 않은 목록 예상 지출
+  const futureExpenditure = () => {};
+
+  const currentExpenditure = () => {};
 
   return (
     <ShoppingListContainer>
@@ -88,25 +153,16 @@ const ShoppingList = ({ year, month, date }) => {
       <ShoppingListTitle>쇼핑 목록 +</ShoppingListTitle>
       <ScrollBox>
         <UncheckedList>
+          {itemList.map((item) => {
+            return <ShoppingItem key={item.id} item={item} shoppingListUnchecked={shoppingListUnchecked} dateToString={dateToString} />;
+          })}
           <ListItem>
-            <ShowItem>
-              <CheckBox type='checkbox' />
-              <ItemPriceContainer>
-                <ItemName>고구마</ItemName>
-                <ItemPrice>5,500원</ItemPrice>
-              </ItemPriceContainer>
-            </ShowItem>
-            <UnshowItem>
-              <PencilIcon icon={faPencil} />
-              <XIcon icon={faX} />
-            </UnshowItem>
-          </ListItem>
-          <ListItem>
-            <CheckBox type='checkbox' />
-            <ItemPriceContainer>
-              <ItemName>입력해주세요</ItemName>
-              <ItemPrice>-- 원</ItemPrice>
-            </ItemPriceContainer>
+            <ItemPriceContainerForm onSubmit={itemSubmitHandler}>
+              <ItemInput id='item' placeholder='입력해주세요.' onChange={itemChangeHandler} />
+              <ItemPriceInputContainer>
+                <ItemPriceInput id='itemPrice' placeholder='---' onChange={priceChangeHandler} />원
+              </ItemPriceInputContainer>
+            </ItemPriceContainerForm>
             <CheckIcon onClick={addItem} icon={faCheck} />
             <XIcon icon={faX} />
           </ListItem>
@@ -117,19 +173,11 @@ const ShoppingList = ({ year, month, date }) => {
         </UncheckedList>
         <ShoppingListTitle>쇼핑 완료</ShoppingListTitle>
         <CheckedList>
-          <ListItem>
-            <ShowItem>
-              <CheckBox type='checkbox' />
-              <ItemPriceContainer>
-                <ItemName>당근</ItemName>
-                <ItemPrice>3,500원</ItemPrice>
-              </ItemPriceContainer>
-            </ShowItem>
-            <UnshowItem>
-              <PencilIcon icon={faPencil} />
-              <XIcon icon={faX} />
-            </UnshowItem>
-          </ListItem>
+          <div>
+            {checkedItemList.map((item) => {
+              return <ShoppingItem key={item.id} item={item} shoppingListChecked={shoppingListChecked} dateToString={dateToString} onRemoveClick={item.onRemoveClick} />;
+            })}
+          </div>
           <CheckListTotalContainer>
             <CheckListTotalText>합계</CheckListTotalText>
             <CheckListTotal>244,600원</CheckListTotal>
--- src/redux/modules/shoppingListActions.js
+++ src/redux/modules/shoppingListActions.js
@@ -1,69 +1,137 @@
-import { db } from '../../firebase';
+import { v4 as uuidv4 } from 'uuid';
 
-const LOAD_LIST = 'LOAD_LIST';
+//  Action Value
 const ADD_LIST = 'ADD_LIST';
 const DELETE_LIST = 'DELETE_LIST';
+const LOAD_LIST = 'LOAD_LIST';
+const MODIFY_MODE_LIST = 'MODIFY_MODE_LIST';
+const UPDATE_LIST = 'UPDATE_LIST';
+const CHECK_LIST = 'CHECK_LIST';
 
-// Action Creators - load list
-export const loadList = (shoppingList) => {
-  return { type: LOAD_LIST, shoppingList };
-};
-
-// Firestore에서 collection을 가져옴
-// const list_db = firestore.collection('shoppingList');
-// const list_db = db.collection('shoppingList');
-
-const initialState = [
-  {
-    id: 12,
-    title: '복숭아',
-    date: '22.12.19',
-    price: 1000,
-    buy: false,
-  },
-  {
-    id: 13,
-    title: '감자',
-    date: '22.12.19',
-    price: 4000,
-    buy: false,
-  },
-];
-
+// Action Creator
 // LIST를 추가하는 Action Creator
-export const addList = (shoppingList) => {
+export const addList = (payload) => {
   return {
     type: ADD_LIST,
-    shoppingList,
+    payload,
   };
 };
 
 // LIST를 삭제하는 Action Creator
-export const deleteList = (shoppingList) => {
+export const deleteList = (payload) => {
   return {
     type: DELETE_LIST,
-    shoppingList,
+    payload,
   };
 };
 
-// Reducer
-export default function reducer(state = initialState, action = {}) {
+// LIST를 가져오는 Action Creator
+export const loadList = (payload) => {
+  return { type: LOAD_LIST, payload };
+};
+
+// LIST를 수정중인지 확인하는 Action Creator
+export const modifyModeList = (payload) => {
+  return {
+    type: MODIFY_MODE_LIST,
+    payload,
+  };
+};
+
+// LIST를 수정하는 Action Creator
+export const updateList = (payload) => {
+  return { type: UPDATE_LIST, payload };
+};
+
+// LIST에 체크하는 Action Creator
+export const checkList = (payload) => {
+  return { type: CHECK_LIST, payload };
+};
+
+// Initial State
+const initialState = [
+  {
+    id: uuidv4(),
+    name: '복숭아',
+    date: '20221219',
+    price: 1000,
+    isChecked: false,
+    modify: false,
+  },
+  {
+    id: uuidv4(),
+    name: '감자',
+    date: '20221219',
+    price: 4000,
+    isChecked: true,
+    modify: false,
+  },
+];
+
+const lists = (state = initialState, action) => {
   switch (action.type) {
-    // do reducer stuff
-    case 'LOAD_LIST':
-      // Firestore에 데이터가 있을때 리턴
-      if (action.shoppingList.length > 0) {
-        return { shoppingList: action.shoppingList };
-      }
-      // 없으면 initialState를 리턴해줌
-      return state;
-
-    case 'DELETE_LIST':
-      const deleted_shopping_list = state.shoppingList.filter((item, index) => index !== action.index);
-      // console.log(deleted_shopping_list);
-      return { shoppingList: deleted_shopping_list };
-
+    case ADD_LIST:
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          name: action.payload.name,
+          date: action.payload.date,
+          price: action.payload.price,
+          isChecked: action.payload.isChecked,
+        },
+      ];
+    case DELETE_LIST:
+      return state.filter((item) => {
+        return item.id !== action.payload;
+      });
+    case LOAD_LIST:
+      return state.map((item) => {
+        if (item.date === action.payload) {
+          return {
+            ...item,
+            modify: !item.modify,
+          };
+        } else {
+          return item;
+        }
+      });
+    case MODIFY_MODE_LIST:
+      return state.map((item) => {
+        if (item.id === action.payload) {
+          return {
+            ...item,
+            modify: !item.modify,
+          };
+        } else {
+          return item;
+        }
+      });
+    case UPDATE_LIST:
+      return state.map((item) => {
+        if (item.id === action.payload.id) {
+          return {
+            ...item,
+            item: action.payload.item,
+          };
+        } else {
+          return item;
+        }
+      });
+    case CHECK_LIST: {
+      const newState = state.map((item) => {
+        return item.id === action.payload.id
+          ? {
+              ...item,
+              isChecked: !item.isChecked,
+            }
+          : item;
+      });
+      return newState;
+    }
     default:
       return state;
   }
-}
+};
+
+export default lists;
