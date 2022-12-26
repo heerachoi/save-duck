097b2e6beb168a26cdb2eefb3fb6b3b01496a54a
heerachoi <heera7722@gmail.com>
2022-12-26 22:30:14 +0900

    [fix] 스펠링 에러

--- src/Component/modal/Modal.jsx
+++ src/Component/modal/Modal.jsx
@@ -1,14 +1,5 @@
-import React from "react";
-import {
-  StyledProfileButton,
-  StyledLogoutButton,
-  StyledVector,
-  Container,
-  ProfileImageContainer,
-  CameraContainer,
-  CameraImage,
-  StyledprofileName,
-} from "./Modal.js";
+import React from 'react';
+import { StyledProfileButton, StyledLogoutButton, StyledVector, Container, ProfileImageContainer, CameraContainer, CameraImage, StyledprofileName } from './Modal.js';
 
 export default function Modal() {
   const onFileChange = (event) => {
@@ -17,39 +8,34 @@ export default function Modal() {
     reader.readAsDataURL(theFile);
     reader.onloadend = (finishedEvent) => {
       const imgDataUrl = finishedEvent.currentTarget.result;
-      localStorage.setItem("imgDataUrl", imgDataUrl);
-      document.getElementById("profileView").src = imgDataUrl;
+      localStorage.setItem('imgDataUrl', imgDataUrl);
+      document.getElementById('profileView').src = imgDataUrl;
     };
   };
   return (
-    <>
+    <div>
       <Container>
         <ProfileImageContainer>
-          <label htmlFor="imgInput">
-            <img src="blankProfiles.png" id="profileView" />
-            <input
-              type="file"
-              id="imgInput"
-              accept="image/*"
-              onChange={onFileChange}
-            />
+          <label htmlFor='imgInput'>
+            <img src='blankProfiles.png' id='profileView' />
+            <input type='file' id='imgInput' accept='image/*' onChange={onFileChange} />
           </label>
         </ProfileImageContainer>
         <CameraContainer>
-          <CameraImage src="camera.png" alt="" />
+          <CameraImage src='camera.png' alt='' />
         </CameraContainer>
         <StyledprofileName>
-          AppleDuck <StyledVector src="Vector.png" />
+          AppleDuck <StyledVector src='Vector.png' />
         </StyledprofileName>
         <StyledProfileButton
           onClick={() => {
-            alert("변경 완료");
+            alert('변경 완료');
           }}
         >
           프로필변경
         </StyledProfileButton>
         <StyledLogoutButton>로그아웃</StyledLogoutButton>
       </Container>
-    </>
+    </div>
   );
 }
