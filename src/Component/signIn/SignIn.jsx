f849176dc2f9845cb5308fdff923cb0f729f349a
heerachoi <heera7722@gmail.com>
2022-12-28 14:30:05 +0900

    [fix] Sign in 에러 - 괄호매칭

--- src/Component/signIn/SignIn.jsx
+++ src/Component/signIn/SignIn.jsx
@@ -5,7 +5,7 @@ import { UserAuth } from '../../context/AuthContext';
 import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signOut, GithubAuthProvider, signInWithPopup } from 'firebase/auth';
 import {
   // Img,
-  Div,
+  Box,
   LoginWrap,
   LoginContaier,
   GitImgContainer,
@@ -25,7 +25,7 @@ import {
   GithubBtn,
   GoogleBtn,
   SocialLoginBtn,
-} from "./SignIn.js";
+} from './SignIn.js';
 
 //더미
 
@@ -40,46 +40,8 @@ const SignInComponent = () => {
   const [emailValid, setEmailValid] = useState(false);
   const [passwordValid, setPasswordValid] = useState(false);
   const [notAllow, setNotAllow] = useState(false);
-  const [passwordleng, setPasswordleng] = useState(false);
-  const [emailleng, setEmailleng] = useState(false);
-  // <div>
   const navigate = useNavigate();
   const { signIn } = UserAuth();
-  // 처음에는 false이고 나중에 사용자 존재 판명이 모두 끝났을 때 true를 통해 해당 화면을 render
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // useEffect(() => {
-  //   authService.onAuthStateChanged((user) => {
-  //     // user 판명을 듣고
-  //     if (user) {
-  //       // 있으면
-  //       setIsLoggedIn(true); // 로그인 됨
-  //     } else {
-  //       setIsLoggedIn(false); // 로그인 안됨
-  //     }
-  //     setInit(true); // user 판명 끝
-  //   });
-  // }, [user]);
-
-  // onAuthStateChanged(authService, (currentUser) => {
-  //   console.log(onAuthStateChanged);
-  //   setUser(currentUser);
-  // });
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(authService, (user) => {
-  //     console.log("test");
-  //     if (user) {
-  //       console.log("user");
-  //       console.log(user);
-  //       setUser({ id: user.uid, email: "" });
-  //     } else {
-  //       console.log("null");
-  //       setUser(null);
-  //     }
-  //   });
-  //   return unsubscribe;
-  // }, []);
-  // </div>
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
@@ -91,12 +53,11 @@ const SignInComponent = () => {
     if (email.length === 0 || password.length === 0) {
       if (email.length === 0 || password.length === 0) {
         setNotAllow(true);
-
         return;
       }
       setNotAllow(false);
-    }, [email, password]);
-
+    }
+  }, [email, password]);
 
   const handleEmail = (e) => {
     // 이메일 정규식
@@ -124,15 +85,11 @@ const SignInComponent = () => {
     let data;
     try {
       const response = await signInWithEmailAndPassword(authService, email, password);
-      // const docRef = await addDoc(collection(data, "users"), {
-      //   email: "email",
-      // });
       window.location.href = '/';
     } catch (error) {
       alert('등록되지않은 아이디입니다.');
     }
   };
-  // console.log("email:", email, "passord:", password);
 
   //소셜로그인
   const onSocialClick = async (event) => {
@@ -163,41 +120,22 @@ const SignInComponent = () => {
     }
   };
   return (
-        <Box></Box>
     <LoginWrap>
+      <Box></Box>
       <LoginContaier>
         <H2>Login</H2>
-
         <form onSubmit={handleSubmit}>
           <EmaillWrap>
             <InputTitle>이메일 주소</InputTitle>
             <InputWrap>
-              <Input
-                type="email"
-                name="email"
-                placeholder="saveduck@saveduck.com"
-                required
-                value={email}
-                onChange={handleEmail}
-              />
+              <Input type='email' name='email' placeholder='saveduck@saveduck.com' required value={email} onChange={handleEmail} />
             </InputWrap>
-            <ErrorMessgeWrap>
-              {!emailValid && email.length > 0 && (
-                <div>! 옳바른 아이디를 입력해주세요.</div>
-              )}
-            </ErrorMessgeWrap>
+            <ErrorMessgeWrap>{!emailValid && email.length > 0 && <div>! 옳바른 아이디를 입력해주세요.</div>}</ErrorMessgeWrap>
           </EmaillWrap>
           <PasswordWrap>
             <InputTitle>비밀번호</InputTitle>
             <InputWrap>
-              <Input
-                type="password"
-                name="password"
-                placeholder="비밀번호를 입력해주세요."
-                required
-                value={password}
-                onChange={handlePassword}
-              />
+              <Input type='password' name='password' placeholder='비밀번호를 입력해주세요.' required value={password} onChange={handlePassword} />
             </InputWrap>
           </PasswordWrap>
         </form>
@@ -208,12 +146,11 @@ const SignInComponent = () => {
             <SignUpBtn onClick={gotoSignup}>회원가입</SignUpBtn>
           </ButtonSign>
           <SocialLoginBtn>
-            <GoogleBtn onClick={onSocialClick} name="google">
+            <GoogleBtn onClick={onSocialClick} name='google'>
               <GogImgContainer />
               Google 로그인
             </GoogleBtn>
-
-            <GithubBtn onClick={onSocialClick} name="github">
+            <GithubBtn onClick={onSocialClick} name='github'>
               <GitImgContainer />
               Github 로그인
             </GithubBtn>
