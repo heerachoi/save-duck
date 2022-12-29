fa4618d3e38133369e22137f4b145051d8ec6d25
heerachoi <heera7722@gmail.com>
2022-12-30 01:07:58 +0900

    [fix] 로그인, 회원가입 에러해결

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -7,38 +7,23 @@ import SignIn from '../pages/SignIn.jsx';
 import SignUp from '../pages/SignUp.jsx';
 import ShopGuidePosting from '../pages/ShopGuidePosting.jsx';
 import ShopGuideArticle from '../pages/ShopGuideArticle.jsx';
-import ShopGuidePostFormEdit from '../Component/shopGuidePostFormEdit/ShopGuidePostFormEdit.jsx';
-import { AuthContextProvider } from '../context/AuthContext.js';
-import ProtectedRoute from './ProtectedRoute.js';
+
+// import EditForm from '../Component/shopGuide/ShopGuidePostFormEdit';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <AuthContextProvider>
-        <Navbar />
-
-        <Routes>
-          <Route path='/' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-        </Routes>
-
-        <Routes>
-          <Route
-            path='/home'
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path='/shopguide' element={<ShopGuide />} />
-          <Route path='/shopguidearticle' element={<ShopGuideArticle />} />
-          <Route path='/shopguidearticle/:id' element={<ShopGuideArticle />} />
-          <Route path='/shopguidedetails/:id' component={ShopGuideDetails} element={<ShopGuideDetails />} />
-          <Route path='/shopguideposting' element={<ShopGuidePosting />} />
-          <Route path='/shopguidepostingEdit/:id' element={<ShopGuidePostFormEdit />} />
-        </Routes>
-      </AuthContextProvider>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<SignIn />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/shopguide' element={<ShopGuide />} />
+        <Route path='/shopguidearticle' element={<ShopGuideArticle />} />
+        <Route path='/shopguidearticle/:id' element={<ShopGuideArticle />} />
+        <Route path='/shopguidedetails/:id' component={ShopGuideDetails} element={<ShopGuideDetails />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/shopguideposting' element={<ShopGuidePosting />} />
+      </Routes>
     </BrowserRouter>
   );
 };
